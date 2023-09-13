7cc1dbdea1a8a8602e3a0aaabf64a779588fe32c
comrade2.0 <68880848+Remo17@users.noreply.github.com>
2023-09-14 00:00:38 +0200

    Add files via upload

---
+++ Classes and Objects/comrade2.0.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+class Point{
+    double x;
+    double y;
+
+    Point(x= 0, y= 0)
+
+
+
+};
+
+
+int Main(){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Phone{
+    string p_name;
+    string p_size;
+
+public:
+
+    Phone(string name,string size){
+    p_name = name;
+    p_size = size;
+    }//phone
+
+    void Makecall(){
+    cout<<"Making call using" << p_name<< endl;
+    }//v1
+
+    void recievecall(){
+    cout<<"Recieve call using" << p_name<< endl;
+    }//v2
+
+};//end of class
+
+int main(){
+
+
+Phone iPhone("IphoneX","1000X500");
+
+iPhone.Makecall() ;
+iPhone.recievecall();
+
+Phone Google_Pixel("G Pixel","500X400");
+
+Google_Pixel.Makecall();  */
+
+
+
+
+
+
+return 0;
+}
